1620e9f6c6915de343a52f6525a00ecdb3809353
Mehnaaz-ops <61306103+Mehnaaz-ops@users.noreply.github.com>
2024-01-02 19:02:45 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 0.14.0"
+  # required_providers {
+  #   openstack = {
+  #     source  = "terraform-provider-openstack/openstack"
+  #     # version = "~> 1.35.0"
+  #   }
+  # }
+}
+
+# Pulled from https://releases.hashicorp.com/terraform-provider-openstack/
+# provider "openstack" {
+#   #version = "(any version)"
+# }
